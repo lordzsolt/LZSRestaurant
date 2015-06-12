1328ff26e21597d3d65991327c2f18d227e98039
Lord Zsolt <Lordzsoltt@gmail.com>
2015-06-13 02:39:00 +0300

    Created Restaurant class

---
+++ Controllers/Restaurant.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lord Zsolt
+ * Date: 13/06/2015
+ * Time: 02:13
+ */
+
+class Restaurant extends MVCControllerBase  {
+
+    public function home() {
+
+    }
+}
