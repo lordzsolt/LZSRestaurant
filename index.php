c08ea389c50b50676b2792ed1a80e0b09fa3a20c
Lord Zsolt <Lordzsoltt@gmail.com>
2015-06-13 02:38:34 +0300

    Started working on routing methods.

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 include "autoloader.php";
 
-?>
+$application = new MVC();
+
+$application->pathPrefix = '/LZSRestaurant';
+
+$application->addRoute(GET, '', ['Restaurant', 'home']);
+$application->addRoute(GET, 'index.php', ['Restaurant', 'home']);
+
+$application->run();
