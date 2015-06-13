e17bfac5367d80366185b0ea77edb60d84fda723
Lord Zsolt <Lordzsoltt@gmail.com>
2015-06-13 16:09:39 +0300

    Created stub header.php

---
+++ Views/header.php
@@ -0,0 +1,40 @@
+<!--
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lord Zsolt
+ * Date: 13/06/2015
+ * Time: 14:00
+ */
+ -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<!-- My CSS -->
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+		<ul class="nav nav-pills navbar-nav navbar-left">
+			<li class="active"><a href="index.html">Menu</a></li>
+			<li><a href="#">My Order</a></li>
+		</ul>
+		<ul class="nav nav-pills navbar-nav navbar-right">
+			<li><a href="#">My Info</a></li>
+			<li><a href="#">Admin</a></li>
+			<li><a href="#">Register</a></li>
+			<li><a href="#">Login</a></li>
+		</ul>
+	</div>
+</nav>
