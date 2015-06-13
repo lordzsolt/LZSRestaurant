78f9c41656ec94ad2e38192289234e8cf213801b
Lord Zsolt <Lordzsoltt@gmail.com>
2015-06-13 16:50:35 +0300

    Added footer.php

--- Controllers/Restaurant.php
+++ Controllers/Restaurant.php
@@ -10,5 +10,7 @@ class Restaurant extends MVCControllerBase  {
 
     public function home() {
         $this->application->view->create('header', null);
+
+        $this->application->view->create('footer', null);
     }
 }
---
+++ Views/footer.php
@@ -0,0 +1,21 @@
+<!--
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lord Zsolt
+ * Date: 13/06/2015
+ * Time: 16:19
+ */
+ -->
+
+
+<footer class="footer">
+    <div class="container white">
+        Lord Zsolt
+    </div>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+</html>
