0213518aa24347170599d6f938abca77285234cf
Lord Zsolt <Lordzsoltt@gmail.com>
2015-06-13 16:06:27 +0300

    Created View component.

---
+++ MVCView.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lord Zsolt
+ * Date: 13/06/2015
+ * Time: 13:49
+ */
+
+class MVCView {
+
+    public function create($path, $vars = null) {
+        $file = 'Views/'.$path.'.php';
+        if (!file_exists($file)) {
+            throw new Exception('The $path argument points to a non-existent template: "'.$path.'".');
+        }
+        if (!empty($vars)) {
+            extract($vars);
+        }
+        include $file;
+    }
+}
