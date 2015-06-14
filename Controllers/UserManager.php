40dad363203760af6d7de8d519b5d0eb589b9db0
Lord Zsolt <Lordzsoltt@gmail.com>
2015-06-15 02:02:52 +0300

    Implemented login and logout

--- Controllers/UserManager.php
+++ Controllers/UserManager.php
@@ -8,8 +8,53 @@
 
 class UserManager extends MVCControllerBase {
 
+    public static function getCookieInfo() {
+        $user = $_SESSION['user'];
+        return [
+            'userId' => $user['userId'],
+            'name' => $user['name'],
+            'username' => $user['username'],
+            'email' => $user['email'],
+            'userType' => $user['userType']
+        ];
+    }
+
     public function login() {
-        parent::createPageWithContent(null);
+        $this->loginWithError();
+    }
+
+    private function loginWithError($error = null) {
+        parent::createPageWithContent(null,
+            function () use ($error) {
+                $this->application->view->create('login', ['error' => $error]);
+            });
+    }
+
+    public function performLogin() {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $stubUser = new User($this->application);
+        $result = $stubUser->getUserWithCredentials($username, $password);
+
+        if (count($result) == 0) {
+            $error = 'Incorrect identifier or password.<p>';
+            goto endOfFunction;
+        }
+
+        endOfFunction:
+        if (isset($error)) {
+            $this->loginWithError($error);
+        }
+        else {
+            $cookieInfo =  $result->getCookieInfo();
+            $_SESSION['user'] = $cookieInfo;
+            parent::createPageWithContent($cookieInfo,
+                function () {
+                    $this->application->view->create('menu', null);
+                    $this->application->view->create('news', null);
+                });
+        }
     }
 
     public function register() {
@@ -17,9 +62,10 @@ class UserManager extends MVCControllerBase {
     }
 
     private function registerWithError($error = null) {
-        parent::createPageWithContent(function () use ($error) {
-            $this->application->view->create('register', ['error' => $error]);
-        });
+        parent::createPageWithContent(null,
+            function () use ($error) {
+                $this->application->view->create('register', ['error' => $error]);
+            });
     }
 
     public function performRegister() {
@@ -60,8 +106,16 @@ class UserManager extends MVCControllerBase {
             return;
         }
 
-        //TODO: Insert and show success form
+        //TODO: Show success form
+    }
 
+    public function performLogout() {
+        $_SESSION['user'] = null;
+        parent::createPageWithContent(null,
+            function () {
+                $this->application->view->create('menu', null);
+                $this->application->view->create('news', null);
+            });
     }
 
     private function validateInput($username, $email, $password, $rePassword) {
@@ -69,6 +123,10 @@ class UserManager extends MVCControllerBase {
 
         if (empty($username) || empty($email) || empty($password) || empty($rePassword)) {
             $error .= 'Please complete all fields.<p>';
+        }
+
+        if (preg_match('/[^a-zA-Z0-9_]/', $username) > 0) {
+            $error .= 'Username contains invalid characters.<p>';
         }
 
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
